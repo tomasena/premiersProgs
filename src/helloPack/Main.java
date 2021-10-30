a735f41f0d3453aece7f921fadeed7880520eb18
tomasena <tomasena@gmail.com>
2021-10-30 18:03:28 +0200

    commit 4

--- src/helloPack/Main.java
+++ src/helloPack/Main.java
@@ -3,7 +3,7 @@ package helloPack;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenus");
+		System.out.println("Bienvenus ");
 		Demenagement dem = new Demenagement("Entreprisse bretonne", 9); // on crée une "instance"
 		dem.auTravail(34); // 34 cartons à  demenager
 
