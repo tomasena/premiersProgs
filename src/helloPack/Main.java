3b2ef7be20304cd9aee918d56695904a3bfdf9ef
tomasena <tomasena@gmail.com>
2021-11-02 16:44:47 +0100

    demenagement avec lecture

--- src/helloPack/Main.java
+++ src/helloPack/Main.java
@@ -1,14 +1,22 @@
 package helloPack;
 
+import java.util.Scanner;
+
 /* Solution authentification github : 
- * https://mycyberuniverse.com/how-fix-fatal-authentication-failed-for-https-github-com.html
+ * https://stackoverflow.com/questions/68790276/pushing-from-eclipse-to-my-github-repository-via-https-stopped-working-git-rec
  */
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenus ");
-		Demenagement dem = new Demenagement("Entreprisse bretonne", 9); // on crée une "instance"
-		dem.auTravail(34); // 34 cartons à  demenager
+
+		Scanner clavier = new Scanner(System.in);
+		System.out.print("Capacité du camion ? ");
+		int cap = clavier.nextInt();
+		System.out.print("Nombre de cartons ? ");
+		int nc = clavier.nextInt();
+		Demenagement dem = new Demenagement("Entreprisse bretonne", cap); // on crée une "instance"
+		dem.auTravail(nc); // 34 cartons à  demenager
 
 	}
 
